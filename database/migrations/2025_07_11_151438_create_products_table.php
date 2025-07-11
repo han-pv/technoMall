9baf3aa352fa95778ff2d5aa60e7e6f7d7c17cff
han-pv <pirliyev17@gmail.com>
2025-07-11 21:08:51 +0500

    first commit

---
+++ database/migrations/2025_07_11_151438_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_model_id')->constrained()->onDelete('cascade');
+            $table->foreignId('color_id')->constrained()->onDelete('cascade');
+            $table->string('slug');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->boolean('is_stock')->default(0);
+            $table->boolean('is_discount')->default(0);
+            $table->integer('discount_precent')->default(0);
+            $table->dateTime('discount_expires_in')->nullable();
+            $table->integer('viewed', 0, 1)->nullable();
+            $table->float('rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
